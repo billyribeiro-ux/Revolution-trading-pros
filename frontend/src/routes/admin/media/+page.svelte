648994f61c349aca462fe16dc6156bbf2a221ccf
Billy Ribeiro <billyribeiro@Billys-MacBook-Pro.local>
2025-12-27 15:42:32 -0500

    Fix ICT11+ TypeScript errors in admin/media page - remaining MediaFile null checks for details panel actions

--- frontend/src/routes/admin/media/+page.svelte
+++ frontend/src/routes/admin/media/+page.svelte
@@ -1269,13 +1269,13 @@
               Responsive Preview
             </button>
           {/if}
-          <button class="btn-primary" onclick={() => handleOptimize(detailItem)}>
+          <button class="btn-primary" onclick={() => detailItem && handleOptimize(detailItem)}>
             <svg viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd"/>
             </svg>
             Optimize
           </button>
-          <button class="btn-danger" onclick={() => handleDelete(detailItem)}>
+          <button class="btn-danger" onclick={() => detailItem && handleDelete(detailItem)}>
             <svg viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
             </svg>
